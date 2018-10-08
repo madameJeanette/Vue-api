7817255ca75168bd7844fd2ce578c3b32a770e3e
Danielle Meerburg <danielle_meerburg@live.nl>
2018-10-08 23:26:14 +0200

    hello world

---
+++ server/routes/api/posts.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//get posts
+router.get('/', (req, res) => {
+    res.send('hello');
+});
+
+//add posts
+
+//delete posts
+
+
+
+module.exports = router;
